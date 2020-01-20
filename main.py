cd2ae21cbe3d5e0252e41e6138aeab7ab4198e69
Yangxin LUO <yasinlaw@outlook.com>
2020-01-20 21:47:34 +0800

    截图显示人脸边框。

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ cameraNumber = 0
 
 count = 0
 countNew = 0
-captureSpan = datetime.timedelta(seconds=3)
+captureSpan = datetime.timedelta(seconds = 2)
 countChangeTime = datetime.datetime.now()
 
 while True:
-    # Capture frame-by-frame
     ret, frame = video_capture.read()
 
     faces = faceCascade.detectMultiScale(
@@ -25,6 +24,9 @@ while True:
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     if countNew != len(faces):
         countChangeTime = datetime.datetime.now()
@@ -42,16 +44,10 @@ while True:
             if count != countNew:
                 print("target insight:" + str(datetime.datetime.now() - countChangeTime))
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # Display the resulting frame
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
