1f8c6c3d0df08c9af6fa32ccfa5694162f03bae5
Yangxin LUO <yasinlaw@outlook.com>
2020-01-20 16:57:42 +0800

    增加截图防抖。

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
-# haarcascade_frontalface_default.xml
-
 import cv2
 import sys
+import datetime
+import uuid
 
 faceCascade = cv2.CascadeClassifier("./env/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_alt2.xml")
 
 video_capture = cv2.VideoCapture(0)
 
+cameraNumber = 0
+
+count = 0
+countNew = 0
+captureSpan = datetime.timedelta(seconds=3)
+countChangeTime = datetime.datetime.now()
+
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     faces = faceCascade.detectMultiScale(
-        gray,
+        frame,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
+
+    if countNew != len(faces):
+        countChangeTime = datetime.datetime.now()
+        countNew = len(faces)
+    else:
+        if datetime.datetime.now() - countChangeTime > captureSpan:
+            if count > countNew:
+                count = countNew
+                print("target exit: " + str(datetime.datetime.now()))
+            elif count < countNew:
+                count = countNew
+                cv2.imwrite("./capture/camera" + str(cameraNumber) + "-" + str(datetime.datetime.now()) + ".png", frame)
+                print("capture!")
+        else:
+            if count != countNew:
+                print("target insight:" + str(datetime.datetime.now() - countChangeTime))
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
